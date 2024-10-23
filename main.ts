86c54e1c41b15eae0c546820775f0c5aec6efd38
mark-lutabi <mark.lutabi@mths.ca>
2024-10-23 11:08:11 -0400

    servo motor

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: mark lutabi
+ * Created on: October, 2024
+ * This program rotates the srvo motor from 30 degrees to 180 degrees
 */
+// variables
+const servoNumber1 = robotbit.Servos.S1
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 30)
+    basic.clearScreen()
+    basic.showString('30')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 145)
+    basic.clearScreen()
+    basic.showString('145')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
